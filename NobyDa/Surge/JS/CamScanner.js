27831c47f7a658cba5a6039d0183bfda3fb24080
xiaoming123m <49673642+xiaoming123m@users.noreply.github.com>
2020-02-29 09:52:50 +0800

    Update CamScanner.js

--- NobyDa/Surge/JS/CamScanner.js
+++ NobyDa/Surge/JS/CamScanner.js
@@ -8,7 +8,7 @@ Quan:  [REWRITE]
 https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url simple-response SFRUUC8xLjEgMjAwIE9LCgp7CiAiZGF0YSI6IHsKICAicHNubF92aXBfcHJvcGVydHkiOiB7CiAgICJleHBpcnkiOiAiMTY0MzczMTIwMCIKICB9CiB9Cn0=
 
 Surge4.0: [Script]
-http-response https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? requires-body=1,max-size=0,script-path=https://github.com/xiaoming123m/QuantumultX/blob/master/NobyDa/Surge/JS/CamScanner.js
+http-response https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
 QX & Q & Surge Mitm = ap*.intsig.net,
 */
 
